fbea5915f580421061c5a4010476e530bd9d2893
Eivintas1 <eivintas.a@gmail.com>
2026-01-10 14:17:54 +0000

    initial commit

---
+++ helloWorld.py
@@ -0,0 +1,40 @@
+#day one python coding
+import math
+
+
+print(2 + 3)
+print(5 - 3)
+print(5 * 3)
+print(700 % 200)
+print(35 / 6)
+print(3 ** 3)
+print( 5 // 6)
+
+your_name = "evan"
+your_family_name = "aleinikovas"
+your_country = "ireland"
+enjoy = "im enjoying 30 days of python"
+
+print(your_country)
+print(your_family_name)
+print(your_name)
+print(enjoy)
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type(your_country))
+print(type(your_family_name))
+print(type(your_name))
+
+#{\displaystyle d(p,q)=|p-q|.}
+#Find an Euclidian distance between (2, 3) and (10, 8)
+
+
+p = (2.3,)
+q = (10.8,)
+
+distance = math.dist(p, q)
+print(distance)
