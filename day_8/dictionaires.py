08ff1a055749a7e2863c123f1914c7a3cc65868a
Eivintas <eivintas.a@gmail.com>
2026-02-04 21:32:19 +0000

    day 8 work

---
+++ day_8/dictionaires.py
@@ -0,0 +1,12 @@
+# curly brackets for dict
+
+dict = {'key 1': 'value1', 'key 2':'value2'}
+
+person = {'first_name':'Evan' ,
+          'last_name':'Aleinikovas',
+          'age': 21,
+          }
+
+print(dict)
+
+print(person)
